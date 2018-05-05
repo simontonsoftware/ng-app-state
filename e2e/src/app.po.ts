26d065b446d22498382b241f92895fbb93191935
Eric <eric@simontonsoftware.com>
2018-05-05 11:24:00 -0400

    chore: copy in integration code

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,7 @@
-import { browser, by, element } from 'protractor';
+import { browser } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { NasModelModule, ngAppStateReducer } from 'ng-app-state';
 import { AppComponent } from './app.component';
+import { IntegrationStore } from './integration-store';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({}, { metaReducers: [ngAppStateReducer] }),
+    NasModelModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  providers: [IntegrationStore],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
