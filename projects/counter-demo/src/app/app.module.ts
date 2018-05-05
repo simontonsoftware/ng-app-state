fd9b8f91a4c46224d12a8a0160e2e139cd24aeeb
Eric <eric@simontonsoftware.com>
2018-05-05 15:25:10 -0400

    chore: copy in unmodified counter demo

--- projects/counter-demo/src/app/app.module.ts
+++ projects/counter-demo/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter';
+import { MyAppComponent } from './my-app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  declarations: [MyAppComponent],
+  bootstrap: [MyAppComponent],
 })
 export class AppModule {}
