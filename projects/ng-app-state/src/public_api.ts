66bef4df8da7ce9c1142fe78a7fda81e48782fe2
ersimont <8042088+ersimont@users.noreply.github.com>
2019-01-19 16:59:33 -0500

    fix: expose `pushToStoreArray()` in the public API

--- projects/ng-app-state/src/public_api.ts
+++ projects/ng-app-state/src/public_api.ts
@@ -6,5 +6,6 @@ export { AppStore } from "./lib/app-store";
 export { ngAppStateReducer } from "./lib/meta-reducer";
 export { StoreObject } from "./lib/store-object";
 export { NasModelModule } from "./lib/nas-model/nas-model.module";
+export { pushToStoreArray } from "./lib/utils/push-to-store-array";
 export { spreadArrayStore$ } from "./lib/utils/spread-array-store";
 export { UndoManager } from "./lib/utils/undo-manager";
