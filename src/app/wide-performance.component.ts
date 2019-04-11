7b726be807b8e860d01b8f2c4bdfdc40d4349a8c
ersimont <8042088+ersimont@users.noreply.github.com>
2019-04-10 22:15:53 -0400

    chore: fixes for manual testing in `WidePerformanceComponent`

--- src/app/wide-performance.component.ts
+++ src/app/wide-performance.component.ts
@@ -11,26 +11,34 @@ class State {
 
 @Component({
   selector: "app-wide-performance",
-  template: "<button (click)='run()'>Wide Performance</button> {{ time }} ms",
+  template: `
+    <button (click)="run()">Wide Performance</button> {{ time }} ms <br />
+    {{ fires }} fires
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WidePerformanceComponent {
   time = 0;
+  fires = 0;
   private store: StoreObject<State>;
 
   constructor(private ngrxStore: Store<any>) {
     this.store = new AppStore(ngrxStore, "testKey", new State());
-    for (let i = width; --i; ) {
-      this.store("array")(i)("counter").$.subscribe();
+    for (let i = width; --i >= 0; ) {
+      this.store("array")(i)("counter").$.subscribe(() => ++this.fires);
     }
   }
 
   run() {
     const start = new Date().getTime();
-    for (let i = width; --i; ) {
-      this.store("array")(i)("counter").set(1);
+    for (let i = width; --i >= 0; ) {
+      this.store("array")(i)("counter").setUsing(increment);
     }
     const end = new Date().getTime();
     this.time = end - start;
   }
+}
+
+function increment(i: number) {
+  return i + 1;
 }
