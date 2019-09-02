7b0aa5555783a1350a8e3433ffdcfc46ba888226
ersimont <8042088+ersimont@users.noreply.github.com>
2019-09-02 15:13:39 -0400

    chore: set up dtslint

---
+++ projects/ng-app-state/src/typing-tests/push-to-store-array.dts-spec.ts
@@ -0,0 +1,6 @@
+import { pushToStoreArray, StoreObject } from "../public-api";
+
+const store = (null as unknown) as StoreObject<Array<Date>>;
+
+// $ExpectType StoreObject<Date>
+pushToStoreArray(store, new Date());
