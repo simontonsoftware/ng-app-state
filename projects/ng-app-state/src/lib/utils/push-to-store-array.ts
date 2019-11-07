4c6fbb94e9e30bd02570d7ad659ea69c6f8a6f63
ersimont <8042088+ersimont@users.noreply.github.com>
2019-11-06 20:49:38 -0500

    chore: fix typo in docs. Closes #26

--- projects/ng-app-state/src/lib/utils/push-to-store-array.ts
+++ projects/ng-app-state/src/lib/utils/push-to-store-array.ts
@@ -6,7 +6,7 @@ import { StoreObject } from "../store-object";
  * ```ts
  * declare const store: StoreObject<number[]>; // assume store starts with [1, 2]
  * const added = pushToStoreArray(store, 3); // store has [1, 2, 3]
- * add.set(17); // store has [1, 2, 17]
+ * added.set(17); // store has [1, 2, 17]
  * ```
  */
 export function pushToStoreArray<T>(store: StoreObject<T[]>, item: T) {
